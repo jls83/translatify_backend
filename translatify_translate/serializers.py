b09a29abc8cd7c6ec7a3b5447d1f84c97fb7571c
U-janeway\jls83 <josephlsanders@gmail.com>
2018-04-19 21:06:12 -0400

    Alleged diff?

--- translatify_translate/serializers.py
+++ translatify_translate/serializers.py
@@ -3,12 +3,14 @@ from translatify_translate.models import TranslatedPhrase, PhraseRequest
 
 
 class TranslatedPhraseSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = TranslatedPhrase
         fields = ('id', 'input_phrase', 'input_language', 'output_phrase')
 
 
 class PhraseRequestSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = PhraseRequest
         fields = ('id', 'requested_phrase', 'requesting_user', 'cache_hit', 'time_requested')
